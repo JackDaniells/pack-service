6d47b5f16a826da6f1d77e234111ddb98d03315b
Daniel Kock <danielk.kock@gmail.com>
2023-12-08 21:30:33 -0300

    test: add pack repository

--- domain/service/service_test.go
+++ domain/service/service_test.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"github.com/JackDaniells/pack-service/domain/contracts"
+	"github.com/JackDaniells/pack-service/domain/contracts/mocks"
 	"github.com/JackDaniells/pack-service/domain/entity"
 	"reflect"
 	"testing"
@@ -10,15 +12,28 @@ func Test_packService_Calculate(t *testing.T) {
 	type args struct {
 		orderItems int
 	}
+
+	type fields struct {
+		packRepository contracts.PackRepository
+	}
+
 	tests := []struct {
-		name string
-		args args
-		want []entity.Pack
+		name   string
+		args   args
+		fields fields
+		want   []entity.Pack
 	}{
 		{
 			name: "Should return 1 250-pack for 1 order item",
 			args: args{
 				orderItems: 1,
+			},
+			fields: fields{
+				packRepository: func() contracts.PackRepository {
+					packService := &mocks.PackRepository{}
+					packService.On("GetAll").Return([]int{250, 500, 1000, 2000, 5000})
+					return packService
+				}(),
 			},
 			want: []entity.Pack{
 				{
@@ -32,6 +47,13 @@ func Test_packService_Calculate(t *testing.T) {
 			args: args{
 				orderItems: 250,
 			},
+			fields: fields{
+				packRepository: func() contracts.PackRepository {
+					packService := &mocks.PackRepository{}
+					packService.On("GetAll").Return([]int{250, 500, 1000, 2000, 5000})
+					return packService
+				}(),
+			},
 			want: []entity.Pack{
 				{
 					Size:     250,
@@ -43,6 +65,13 @@ func Test_packService_Calculate(t *testing.T) {
 			name: "Should return 1 500-pack and 1 250-pack for 501 order items",
 			args: args{
 				orderItems: 501,
+			},
+			fields: fields{
+				packRepository: func() contracts.PackRepository {
+					packService := &mocks.PackRepository{}
+					packService.On("GetAll").Return([]int{250, 500, 1000, 2000, 5000})
+					return packService
+				}(),
 			},
 			want: []entity.Pack{
 				{
@@ -59,6 +88,13 @@ func Test_packService_Calculate(t *testing.T) {
 			name: "Should return 2 5000-pack, 1 2000-pack and 1 250-pack for 12001 order item",
 			args: args{
 				orderItems: 12001,
+			},
+			fields: fields{
+				packRepository: func() contracts.PackRepository {
+					packService := &mocks.PackRepository{}
+					packService.On("GetAll").Return([]int{250, 500, 1000, 2000, 5000})
+					return packService
+				}(),
 			},
 			want: []entity.Pack{
 				{
@@ -78,7 +114,9 @@ func Test_packService_Calculate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := &packService{}
+			p := &packService{
+				repository: tt.fields.packRepository,
+			}
 			got := p.Calculate(tt.args.orderItems)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Calculate() got = %v, want %v", got, tt.want)
